1239ca9d307367075c4701b677fe9671a054b46e
Prashant <invizableprash@gmail.com>
2026-03-29 14:11:26 +0530

    caesar Python implementation

--- caesar/python/main.py
+++ caesar/python/main.py
@@ -1,6 +1,71 @@
+import sys
+
+
 def main():
-    print("Hell world")
+    args = sys.argv
+    # Makesure enough arguments are provided
+    if len(args) != 3:
+        print(f"Usage: {args[0]} <input> <output>")
+        return 1
+
+    # Try to read fro input file
+    try:
+        with open(args[1]) as source:
+            text = source.read()
+    except FileNotFoundError:
+        print(f"File {args[1]} not found. Makesure you've entered correct name")
+        return 1
+
+    if len(text) == 0:
+        print("Input file cannot be empty.")
+        return 1
+
+    # Opening/creating output file
+    out = open(args[2], "w")
+
+    # Try to get key from the user
+    try:
+        key = int(input("Enter key: "))
+    except ValueError:
+        print("Invalid key.")
+        out.close()
+        return 1
+
+    # Encrypt text
+    enc = encrypt(text, key)
+
+    # Show the user plain text and encrypted text
+    print(f"Plaintext:\n\t{text}")
+    print(f"Encrypted:\n\t{enc}")
+
+    # Writing to the output file and closing it
+    out.write(enc)
+    out.close()
+    return 0
 
 
+def encrypt(text, key):
+    # Setting up some vriables
+    ascii_A = ord("A")
+    ascii_a = ord("a")
+    enc = []
+    # Going through each character in text
+    for c in text:
+        # If character is uppercase
+        if c.isupper():
+            ap = (((ord(c) - ascii_A) + key) % 26) + ascii_A
+            enc.append(chr(ap))
+        # If character is lowercase
+        elif c.islower():
+            ap = (((ord(c) - ascii_a) + key) % 26) + ascii_a
+            enc.append(chr(ap))
+        # If character isn't alphabet
+        else:
+            enc.append(c)
+    # Join list into a string and return
+    return "".join(enc)
+
+
+# Calling the main function
 if __name__ == "__main__":
     main()
