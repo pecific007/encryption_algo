35215b9989858e2cad745cfb55a4c08c91b28380
Prashant <invizableprash@gmail.com>
2026-04-27 18:16:49 +0530

    Full caesar impl in Rust without cmd-args

--- caesar/rust/main.rs
+++ caesar/rust/main.rs
@@ -1,10 +1,27 @@
 // Command to run this: `$ rustc main.rs && ./main`
-fn main() {
-    let plain_text = "Hello world";
-    let key: u8 = 1;
-    let enc = encrypt(&plain_text, key);
-    println!("Plaintext: {plain_text}");
+
+use std::fs::File;
+use std::io::prelude::*;
+
+#[allow(unused_assignments)]
+fn main() -> std::io::Result<()> {
+    let mut ret = Ok(());
+    let mut src = File::open("../plain_text.txt")?;
+    let mut content = String::new();
+    src.read_to_string(&mut content)?;
+    let mut key = Default::default();
+    println!("Enter key: ");
+    std::io::stdin()
+        .read_line(&mut key)
+        .expect("Couldn't process input!");
+    let key: u8 = key.trim().parse().expect("The key must be an integer");
+    let key = key % 26;
+    let enc = encrypt(&content, key);
+    println!("Plaintext: {content}");
     println!("Encrypted: {enc}");
+    let mut out = File::create("out.txt")?;
+    ret = out.write_all(enc.as_bytes());
+    ret
 }
 
 fn encrypt(text: &str, key: u8) -> String {
